9e9209e3563cae1c3d059bd724c6a4a1c3ceef5d
Tanmai Kamat <tanmaikamat@gmail.com>
2022-05-03 21:45:41 +0530

    Basic Drag Drop created
    Baisc Drag and Drag mechanism designed

---
+++ Assets/BoneDragScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoneDragScript : MonoBehaviour
+{
+    public enum bones{
+        Skull,Rib
+    }
+
+
+
+
+
+    [SerializeField]
+    Transform myParent;
+    [SerializeField]
+    bones thisBone;
+    bool choose=true,drop=false;
+    Vector3 initialPosition;
+    void Start()
+    {
+        
+        initialPosition=transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!choose){
+                 myParent.position=Camera.main.ScreenToWorldPoint(Input.mousePosition+new Vector3(0,0,10));
+       
+        }
+       
+
+        
+    }
+    void OnMouseDown(){
+    Debug.Log("Sprite Clicked");
+     if(choose){
+            choose=false;
+            drop=true;
+
+        }else if(drop){
+            choose=true;
+            drop=false;
+            Debug.Log(thisBone+"Point");
+            Vector3 finalPos=FindObjectOfType<GameManager>().GetBonePos(thisBone+"Point");
+            Debug.Log(Vector3.Distance(transform.position,finalPos));
+            if(Vector3.Distance(transform.position,finalPos)<0.5){
+                myParent.position=finalPos;
+            }else{
+                myParent.position=initialPosition;
+            }
+        }
+}
+}
+
---
+++ Assets/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    GameObject Holder;
+    Dictionary<string,Transform> bonePositions;
+    void Start()
+    {
+        bonePositions=new Dictionary<string, Transform>();
+        foreach(Transform t in Holder.GetComponentsInChildren<Transform>()){
+            Debug.Log(t.gameObject.name.ToString().Equals("SkullPoint"));
+            bonePositions.Add(t.gameObject.name.ToString(),t);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public Vector3 GetBonePos(string t){
+        Debug.Log("Bone :"+bonePositions[t]);
+        return bonePositions[t].position;
+    }
+}
