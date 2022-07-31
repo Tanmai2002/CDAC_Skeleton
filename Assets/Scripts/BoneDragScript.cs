3cb452324e8e8e2cbfd98ea8d9e60819873c4786
Pradhyuman <superpradhyuman@gmail.com>
2022-07-31 17:51:10 +0530

    new bones added
    4 new bones added

--- Assets/Scripts/BoneDragScript.cs
+++ Assets/Scripts/BoneDragScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class BoneDragScript : MonoBehaviour
 {
     public enum bones{
-        Skull,RibCage,Femur_Left,Femur_Right,Clavical_Right,Clavical_Left,Fingers_Right, Fingers_Left,Humerous_Left, Humerous_Right,Ulna_Left, Ulna_Right, Rib_Center,Foot_Left,Foot_Right,Pelvis, Vertebrate_Column,Radius_Left, Radius_Right, Tibia_Left, Tibia_Right
+        Skull,RibCage,Femur_Left,Femur_Right,Clavical_Right,Clavical_Left,Fingers_Right, Fingers_Left,Humerous_Left, Humerous_Right,Ulna_Left, Ulna_Right, Rib_Center,Foot_Left,Foot_Right,Pelvis, Vertebrate_Column,Radius_Left, Radius_Right, Tibia_Left, Tibia_Right, Scapula_Left, Scapula_Right,Patella_Left, Patella_Right
         }
     bool isFixed;
     [SerializeField]
