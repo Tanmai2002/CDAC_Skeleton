0c04954a977c4862a1bde0a548e86944209e2c7c
jinish08 <jinishshah08@gmail.com>
2022-08-05 13:38:23 +0530

    made the navigation for game 3

--- Assets/Scripts/Navigate.cs
+++ Assets/Scripts/Navigate.cs
@@ -12,6 +12,9 @@ public class Navigate : MonoBehaviour
     public void Level2(){
         SceneManager.LoadScene(2);
     }
+    public void Level3(){
+        SceneManager.LoadScene(3);
+    }
     public void BackHome(){
         SceneManager.LoadScene(0);
     }
